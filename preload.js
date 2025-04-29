87f303e306433ffb389aa554f4ac0c1191e73f22
Multarix <darien2105@gmail.com>
2025-04-30 01:06:10 +1000

    updateDifficulty renamed to updateChallenge

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron/renderer');
 contextBridge.exposeInMainWorld('electronAPI', {
 	openFile: () => ipcRenderer.invoke('openFile'),
 	submitForm: (formData) => ipcRenderer.invoke('submitForm', formData),
-	updateDifficulty: (data) => ipcRenderer.invoke("updateDifficulty", data),
+	updateChallenge: (data) => ipcRenderer.invoke("updateChallenge", data),
 	deleteChallenge: (data) => ipcRenderer.invoke("deleteChallenge", data),
 	syncToServer: () => ipcRenderer.invoke("syncToServer")
 });
