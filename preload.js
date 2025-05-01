d88fbced73c7ec536e2f7b69fbbf9ef81526da43
Multarix <darien2105@gmail.com>
2025-05-02 02:33:08 +1000

    Add getAuth

--- preload.js
+++ preload.js
@@ -1,12 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron/renderer');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+	setAuth: (auth) => ipcRenderer.invoke("setAuth", auth),
+	getAuth: () => ipcRenderer.invoke("getAuth"),
 	getSaveLocation: () => ipcRenderer.invoke("getSaveLocation"),
 	openFile: () => ipcRenderer.invoke('openFile'),
 	submitForm: (formData) => ipcRenderer.invoke('submitForm', formData),
 	updateChallenge: (data) => ipcRenderer.invoke("updateChallenge", data),
 	deleteChallenge: (data) => ipcRenderer.invoke("deleteChallenge", data),
 	syncToServer: () => ipcRenderer.invoke("syncToServer"),
-	setAuth: (auth) => ipcRenderer.invoke("setAuth", auth),
 	onUpdateStatus: (callback) => ipcRenderer.on("uploadStatus", (_event, value) => callback(value))
 });
